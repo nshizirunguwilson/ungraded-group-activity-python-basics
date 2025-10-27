b32ea6ca339addd0ea41b659079f0c67be973ef9
Gania-Isaro <g.kayumba@alustudent.com>
2025-10-27 14:45:09 +0200

    Greetings commit

---
+++ greetings.py
@@ -0,0 +1,7 @@
+def show_intro():
+    print("==============================")
+    print(" Welcome to the User Info Program ")
+    print("==============================")
+
+def show_exit_message(): 
+    print("Thank you for using the program. Goodbye!")
